48ad8978d9878b2484a468a8e5d9b4ab2271ffa6
Gitahi <116303227+Gitahi254@users.noreply.github.com>
2024-07-08 01:43:12 -0800

    Update app.js
    I have updated the JavaScript to include the current date in the format d/m/yy

--- Digital clock/app.js
+++ Digital clock/app.js
@@ -1,4 +1,5 @@
 const clock = document.querySelector('.clock');
+const dateDiv = document.querySelector('.date');
 
 const tick = () => {
     const now = new Date();
@@ -8,13 +9,27 @@ const tick = () => {
     const s = now.getSeconds(); //get the seconds
     // console.log(h, m, s);
 
+    const d = now.getDate();
+    const mon = now.getMonth() + 1;
+    const y = now.getFullYear().toString().slice(-2);
+
+    //for clock
     const html = `
     <span>${h}</span> :
     <span>${m}</span> :
     <span>${s}</span>
     `;
 
+    //for date
+    const htmlDate = `
+        <span>${d}</span>/
+        <span>${mon}</span>/
+        <span>${y}</span>.
+    `
+    
+
     clock.innerHTML = html;
+    dateDiv.innerHTML = htmlDate;
 
 
 };
